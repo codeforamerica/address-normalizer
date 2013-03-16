f686b229fbc4676028c572aa62acd5a6c4b85c4c
Dave Guarino <daguar@gmail.com>
2013-03-16 12:38:49 -0700

    Deal with non UTF8 encoding and blank addresses gracefully

--- address_normalizer.rb
+++ address_normalizer.rb
@@ -1,19 +1,37 @@
 require 'csv'
 require 'street_address'
+require 'cmess/guess_encoding'
+require 'pry'
 
 module AddressNormalizer
 
   # Given a CSV file, creates a new one with normalized addresses
   # If there are malformed rows/errors, saves those to a separate output file
   def self.normalize_csv(filename)
+
+    # Overhead setup
     counter = 0
     address_index = 0
     normalized_address_index = 0
     errors = Array.new
     malformed_rows = Array.new
-    timestamp = Time.now.to_s.gsub(/:|-/,"")
+    timestamp = Time.now.to_s.gsub(/:|-/,"").gsub(/\s+/,"_")
     normalized_output_file = File.open("#{File.basename(filename,".*")}_NormalizedAddresses_#{timestamp}.csv", "w")
-    IO.foreach(filename) do |line|
+
+    # Check encoding
+    puts "Opening file to check encoding..."
+    source_file = File.read(filename)
+    source_encoding = CMess::GuessEncoding::Automatic.guess(source_file)
+    puts "Encoding: #{source_encoding}"
+    puts "Total number of rows: #{source_file.lines.count}"
+    source_file = nil
+
+    puts "Normalizing addresses (this may take a while)..."
+
+    IO.foreach(filename, :encoding => source_encoding) do |line|
+
+      puts "On row #{counter}" if counter % 100 == 0
+
       # If the header row, get the index for the address and add a column for the address_normalized
       if counter == 0
         CSV.parse(line) do |row|
@@ -23,20 +41,31 @@ module AddressNormalizer
           normalized_output_file.write(CSV.generate_line(row))
         end
         counter += 1
+
+      # For normal rows, normalize, write to file, and catch errors
       else
         begin
           CSV.parse(line) do |row|
-            sa = StreetAddress::US.parse(row[address_index] + ", , ")
+            begin
+              sa = StreetAddress::US.parse(row[address_index] + ", , ")
+            # Catch empty address
+            rescue NoMethodError
+              sa = ""
+            end
             row << sa.to_s.upcase
             normalized_output_file.write(CSV.generate_line(row))
             counter = counter + 1
           end
+        # Rescue from problem rows
         rescue CSV::MalformedCSVError => er
           errors << er.message
           malformed_rows << line
         end
       end
+
     end
+
+    # If there were problem rows, print them to the user and save them to a file
     unless malformed_rows.empty?
       puts "Errors: #{errors.to_s}\n\n\n" unless errors.empty?
       malformed_row_output_file = File.open("AddressNormalizer_MalformedRows_#{timestamp}.txt", "w")
@@ -50,6 +79,7 @@ module AddressNormalizer
       puts "Malformed rows saved to disk in #{malformed_row_output_file.path}."
     end
     puts "Done!"
+
   end
 
 end
