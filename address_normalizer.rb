fe4fa3bdc253347bbe35d881c9e28bfb0fb4d5ea
Mike Holubowski <mike.holubowski@gmail.com>
2013-06-06 23:01:27 -0700

    pulling out encoding check into own method

--- address_normalizer.rb
+++ address_normalizer.rb
@@ -10,26 +10,22 @@ module AddressNormalizer
   def self.normalize_csv(filename)
 
     # Overhead setup
-    counter = 0
+    counter       = 0
     address_index = 0
     normalized_address_index = 0
-    errors = Array.new
+
+    errors         = Array.new
     malformed_rows = Array.new
-    timestamp = Time.now.to_s.gsub(/:|-/,"").gsub(/\s+/,"_")
+    timestamp      = Time.now.to_s.gsub(/:|-/,"").gsub(/\s+/,"_")
+
     normalized_output_file = File.open("#{File.basename(filename,".*")}_NormalizedAddresses_#{timestamp}.csv", "w")
 
     # Check encoding
-    puts "Opening file to check encoding..."
-    source_file = File.read(filename)
-    source_encoding = CMess::GuessEncoding::Automatic.guess(source_file)
-    puts "Encoding: #{source_encoding}"
-    puts "Total number of rows: #{source_file.lines.count}"
-    source_file = nil
+    source_encoding = check_encoding(filename)
 
     puts "Normalizing addresses (this may take a while)..."
 
     IO.foreach(filename, :encoding => source_encoding) do |line|
-
       puts "On row #{counter}" if counter % 100 == 0
 
       # If the header row, get the index for the address and add a column for the address_normalized
@@ -80,6 +76,20 @@ module AddressNormalizer
     end
     puts "Done!"
 
+  end
+
+  def self.check_encoding filename
+    puts "Opening file to check encoding..."
+    
+    source_file     = File.read(filename)
+    source_encoding = CMess::GuessEncoding::Automatic.guess(source_file)
+    
+    puts "Encoding: #{source_encoding}"
+    puts "Total number of rows: #{source_file.lines.count}"
+    
+    source_file = nil
+
+    return source_encoding
   end
 
 end
